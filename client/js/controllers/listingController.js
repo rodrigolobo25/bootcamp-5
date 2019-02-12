c2b5fe343cfb245caaaf18ecb86303eace108017
rodrigolobo25 <rodrigolobo@ufl.edu>
2019-02-11 19:41:59 -0500

    fhfhhf

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -15,12 +15,14 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
             $scope.newListing = {};
         }
         $scope.deleteListing = function (index) {
+            
             var indexd = $scope.listings.indexOf(index);
+            $scope.listings.splice(indexd, 1);
             Listings.delete(indexd).then(function (response) {
             }, function (error) {
                 console.log('Unable to delete listings:', error);
             });
-            $scope.listings.splice(indexd, 1);
+            
         };
         $scope.showDetails = function (index) {
             $scope.info = index;
