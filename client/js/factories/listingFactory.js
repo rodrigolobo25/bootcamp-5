9b4d3828fff7e7f85ee305ee1370aada53509ba1
rodrigolobo25 <rodrigolobo@ufl.edu>
2019-02-15 17:17:19 -0500

    gdgdgdllo

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -12,7 +12,7 @@ angular.module('listings', []).factory('Listings', function($http) {
 	   /**TODO
         return result of HTTP delete method
        */
-        return $http.delete('https://afternoon-brushlands-68906.herokuapp.com/api/listings/' + id);
+        return $http.delete('https://afternoon-brushlands-68906.herokuapp.com/api/listings/:' + id);
     }
   };
 
